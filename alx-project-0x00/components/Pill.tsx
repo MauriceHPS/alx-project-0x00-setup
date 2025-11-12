0d1f8d590a177a2e7f69eea5342f977e15057a2a
MauriceHPS <maurice@headpoint.info>
2025-11-12 18:04:26 +0300

    Created an interface in a TS file and passed the props to a .tsx file

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,10 +1,11 @@
+import { PillProps } from "@/interfaces";
 import React from "react";
 
-const Pill: React.FC = () => {
+const Pill: React.FC<PillProps> = ({ title }) => {
   return (
     <>
       <div className="flex justify-center items-center bg-[#F9#F9#f9] px-3 w-auto h-[27px] rounded-full">
-        <p className="text-sm">Title</p>
+        <p className="text-sm">{title}</p>
       </div>
     </>
   );
