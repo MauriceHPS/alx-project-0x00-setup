afa9e94e98a7ed60f0e6ffceda1f74d2c3497ee5
MauriceHPS <maurice@headpoint.info>
2025-11-12 17:48:24 +0300

    Added code in Cards.tsx

---
+++ alx-project-0x00/components/Pill.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Pill: React.FC = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center bg-[#F9#F9#f9] px-3 w-auto h-[27px] rounded-full">
+        <p className="text-sm">Title</p>
+      </div>
+    </>
+  );
+};
+
+export default Pill;
