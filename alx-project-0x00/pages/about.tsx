22a1eec5cabbbb39169c69fe74a78f03c3e50013
MauriceHPS <maurice@headpoint.info>
2025-11-12 17:20:29 +0300

    Added landing and about pages in /pages

---
+++ alx-project-0x00/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About: React.FC = () => {
+  return (
+    <>
+      <h1 className="text-xl font-extralight">About Page</h1>
+    </>
+  );
+};
+
+export default About;
